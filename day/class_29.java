715192956c6cdf9bdee65220f59466b0b58445b5
HsiaoCz <984274788@qq.com>
2024-03-12 19:58:36 +0800

    java

---
+++ day/class_29.java
@@ -0,0 +1,35 @@
+package day;
+
+public class class_29 {
+    public static void main(String[] args) {
+        // 面向对象的抽象
+        // 抽象方法：只有声明，没有实现的方法
+        // abstract 返回值类型 方法名(参数)
+
+        // 不完整的类就是抽象类，抽象的类无法实例化的
+        // 抽象类，抽象方法
+        // 类或方法无法准确定义
+        // 抽象类无法直接构建对象，但是可以通过类间接构建对象
+        // 如果抽象类中含有抽象方法，那么子类继承抽象类，需要重写抽象方法，将方法补充完整
+
+        // abstract 关键字不能和final同时使用
+
+        Chinese21 c = new Chinese21();
+        c.eat();
+    }
+}
+
+abstract class Person29 {
+
+    public abstract void eat();
+
+    public void test() {
+
+    }
+}
+
+class Chinese21 extends Person29 {
+    public void eat() {
+        System.out.println("中国人使用筷子吃饭");
+    }
+}
