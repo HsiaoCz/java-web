a574640a8c06f43902161cf21ba62682fce678c1
HsiaoCz <984274788@qq.com>
2024-04-06 11:21:42 +0800

    some

---
+++ collection/set_17_hashtable.java
@@ -0,0 +1,20 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class set_17_hashtable {
+    public static void main(String[] args) {
+        // hashtable
+        Hashtable<String, String> table = new Hashtable<>();
+        HashMap<String, String> map = new HashMap<>();
+
+        map.put(null, null);
+        table.put(null, null);
+        // hashtable和hashmap实现方式不一样
+        // 底层结构和容量不同
+        // hashmap的k,v都可以为null,hashtable的key,value不能为null
+        // HashMap的数据定位采用的是Hash算法，但是Hashtable采用的就是HashCode
+        // hashMap的性能较高，Hashtable的性能较低
+    }
+}
