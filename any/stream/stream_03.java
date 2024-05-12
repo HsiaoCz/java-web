b2b38f364aefa6c02c37c00ab876915172a80264
HsiaoCz <984274788@qq.com>
2024-05-13 05:40:20 +0800

    some

---
+++ any/stream/stream_03.java
@@ -0,0 +1,55 @@
+package any.stream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class stream_03 {
+    public static void main(String[] args) throws IOException {
+        // 文件复制
+        // 数据源文件对象
+        File srcFile = new File("./hello");
+
+        // 数据目的地文件对象
+        File destFile = new File("./hi");
+
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        // 文件输入流
+        try {
+
+            in = new FileInputStream(srcFile);
+            out = new FileOutputStream(destFile);
+
+            // 打开阀门，流转数据 (输入)
+            int data = in.read();
+            // if (data != -1) {
+            // // 打开阀门，流转数据(输出)
+            // out.write(data);
+            // }
+
+            while ((data = in.read()) != -1) {
+                out.write(data);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
