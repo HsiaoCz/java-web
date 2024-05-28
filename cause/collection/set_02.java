ea252246c4eeca215247fd58f49def6129c371bc
HsiaoCz <984274788@qq.com>
2024-05-28 16:52:29 +0800

    some

---
+++ cause/collection/set_02.java
@@ -0,0 +1,17 @@
+package cause.collection;
+
+public class set_02 {
+    public static void main(String[] args) {
+        // 1.collection接口
+        // 2.map接口
+        // 常用的子接口
+        // list:按照插入顺序保存数据，数据可以重复的
+        // arrayList, linkedList
+        // set:集：无序保存，数据不能重复
+        // HashSet
+        // queue；队列
+        // 具体的实现类:ArraryBlockQueue
+        // 2.map
+        // HashMap HashTable
+    }
+}
