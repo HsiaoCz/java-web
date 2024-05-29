c2a9e520860a19cd710fe0c1158a3efab151908c
HsiaoCz <984274788@qq.com>
2024-05-29 14:40:03 +0800

    some

---
+++ cause/collection/set_11.java
@@ -0,0 +1,21 @@
+package cause.collection;
+
+import java.util.HashSet;
+
+public class set_11 {
+    public static void main(String[] args) {
+        // hashset 的常用方法
+
+        HashSet<String> set = new HashSet<>();
+        set.add("zhangsan");
+        set.add("lisi");
+
+        // 判断集合是否为空
+        set.isEmpty();
+        set.clear(); // 清空集合
+        set.contains("zhangsan"); // 判断集合是否包含
+        System.out.println(set.size()); // 集合的大小
+        Object clone = set.clone(); // 克隆
+        System.out.println(clone);
+    }
+}
