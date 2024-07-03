311b9362019ce4cbe9694574f9046cbd5370d6e5
HsiaoCz <984274788@qq.com>
2024-07-04 02:19:44 +0800

    add some

---
+++ cause/some/date_03.java
@@ -0,0 +1,22 @@
+package cause.some;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class date_03 {
+    public static void main(String[] args) {
+        // 日历类
+
+        Calendar instance = Calendar.getInstance();
+        System.out.println(instance);
+        // 获取月份
+        instance.get(Calendar.YEAR);
+        System.out.println(instance.get(Calendar.YEAR));
+        System.out.println(instance.get(Calendar.MONTH));
+        System.out.println(instance.get(Calendar.DATE));
+
+        instance.setTime(new Date());
+        // 将当前的时间加1年
+        instance.add(Calendar.YEAR, 1);
+    }
+}
