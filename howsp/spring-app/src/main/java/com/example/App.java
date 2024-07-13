49dfc8a449806faf4e18c0346d83ef972aec0d96
HsiaoCz <984274788@qq.com>
2024-07-14 07:10:55 +0800

    add some

---
+++ howsp/spring-app/src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class App {
+    public static void main(String[] args) {
+        // SpringApplication.run(SpringAppApplication.class, args);
+        System.out.println("hello my man");
+    }
+}
