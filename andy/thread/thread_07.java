0470935efea47cc9589e5cbffce03f25365f6cc4
HsiaoCz <984274788@qq.com>
2024-07-24 22:15:26 +0800

    add some

---
+++ andy/thread/thread_07.java
@@ -0,0 +1,37 @@
+package andy.thread;
+
+public class thread_07 {
+    public static void main(String[] args) {
+        // 线程安全的问题
+        // 所谓的线程安全问题，就是多个线程在并发执行时，修改了共享内存中对象的属性
+        // 导致的数据冲突问题
+        User7 user = new User7();
+
+        Thread t1 = new Thread(() -> {
+            user.name = "zhangsan";
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(user.name);
+        });
+        Thread t2 = new Thread(() -> {
+            user.name = "lisi";
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(user.name);
+        });
+        t1.start();
+        t2.start();
+
+        System.out.println("main主线程执行完毕...");
+    }
+}
+
+class User7 {
+    public String name;
+}
