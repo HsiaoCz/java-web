b4cd523e07b3d8c8515a6cf406400effb231eb94
HsiaoCz <984274788@qq.com>
2024-07-26 20:39:37 +0800

    add some

--- andy/annotation/annotation_test1.java
+++ andy/annotation/annotation_test1.java
@@ -1,13 +1,7 @@
 package andy.annotation;
 
-public class annotation_test {
-    public static void main(String[] args) {
-
-    }
-}
-
 @MyTest1(aaa = "niumo", ccc = { "HTML", "JAVA" })
-public class anotation_test1 {
+public class annotation_test1 {
     @MyTest1(aaa = "铁扇公主", bbb = false, ccc = { "python", "go" })
     public void test1() {
 
